cd33e93a4808fab2b9187dc609e1084208737842
Kevin Mugele <kevin.mugele@gmail.com>
2021-07-29 16:02:45 -0400

    Write code and tests for finding publication time frame for specific author. Wrote test for helper method.

--- lib/library.rb
+++ lib/library.rb
@@ -15,8 +15,23 @@ class Library
     end
   end
 
-  def publication_time_frame_for(author)
+  def find_publication_year(author)
+    publication_years_per_author = []
+    author.books.each do |book|
+      publication_years_per_author << book.publication_year
+    end
+    publication_years_per_author
   end
+
+  def publication_time_frame_for(author)
+    publication_time_frame = {}
+    publication_time_frame[:start] = find_publication_year(author).sort[0]
+    publication_time_frame[:end] = find_publication_year(author).sort[-1]
+    publication_time_frame
+  end
+
+
+
 
 
 end
